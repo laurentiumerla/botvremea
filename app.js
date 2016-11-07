ce532cc0bde265dbe04e9a3b41e84829ef81d623
Merla <I334691@global.corp.sap>
2016-11-07 15:58:37 +0200

    update

--- app.js
+++ app.js
@@ -23,34 +23,34 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-// bot.dialog('/', new builder.IntentDialog()
-//     .onBegin([
-//         function(session) {
-//             console.log('Session: ', session);
-//             // session.beginDialog('/ensureProfile', session.userData.profile);
-//         },
-//         function(session, results) {
-//             session.userData.profile = results.response;
-//             session.send('Buna %(name)s! Imi place %(location)s!', session.userData.profile);
-//         }
-//     ])
-//     .matches(/^Buna/i, function(session) {
-//         session.send('Buna %(name)s!', session.userData.profile);
-//     })
-//     .onDefault(function(session) {
-//         session.send("Nu inteleg!");
-//     })
-// );
+bot.dialog('/', new builder.IntentDialog()
+    // .onBegin([
+    //     function(session) {
+    //         console.log('Session: ', session);
+    //         // session.beginDialog('/ensureProfile', session.userData.profile);
+    //     },
+    //     function(session, results) {
+    //         session.userData.profile = results.response;
+    //         session.send('Buna %(name)s! Imi place %(location)s!', session.userData.profile);
+    //     }
+    // ])
+    .matches(/^Buna/i, function(session) {
+        session.send('Buna %(name)s!', session.userData.profile);
+    })
+    .onDefault(function(session) {
+        session.send("Nu inteleg!");
+    })
+);
 
-bot.dialog('/', [
-    function(session) {
-        session.beginDialog('/ensureProfile', session.userData.profile);
-    },
-    function(session, results) {
-        session.userData.profile = results.response;
-        session.send('Buna %(name)s! Imi place %(location)s!', session.userData.profile);
-    }
-]);
+// bot.dialog('/', [
+//     function(session) {
+//         session.beginDialog('/ensureProfile', session.userData.profile);
+//     },
+//     function(session, results) {
+//         session.userData.profile = results.response;
+//         session.send('Buna %(name)s! Imi place %(location)s!', session.userData.profile);
+//     }
+// ]);
 
 bot.dialog('/ensureProfile', [
     function(session, args, next) {
