896734c7db0f309b8665eff87d534f499f711c00
Merla <I334691@global.corp.sap>
2016-11-07 15:48:05 +0200

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-// var http = require('http');
 
 //=========================================================
 // Bot Setup
@@ -35,18 +34,6 @@ bot.dialog('/', new builder.IntentDialog()
         }
     ])
     .matches(/^Buna/i, function(session) {
-        // var options = {
-        //     host: 'http://apidev.accuweather.com/currentconditions/v1/1161950.json?language=ro&apikey=hoArfRosT1215'
-        //     // port: 80,
-        //     // path: '/upload',
-        //     // method: 'GET'
-        // };
-        // http.get(options, function(res) {
-        //     console.log("Got response: " + res.statusCode);
-        //     console.log("Response: ", res);
-        // }).on('error', function(e) {
-        //     console.log("Got error: " + e.message);
-        // });
         session.send('Buna %(name)s!', session.userData.profile);
     })
     .onDefault(function(session) {
